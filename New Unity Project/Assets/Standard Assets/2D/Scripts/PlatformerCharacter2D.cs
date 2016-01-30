9b63912aa8f00e7c92455a5933d7a92917088d91
Johnny Pinheiro <pinheiro_dom@hotmail.com.br>
2016-01-30 10:14:52 -0200

    personagem

--- New Unity Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ New Unity Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -20,6 +20,8 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
+        private int loseLives = 0;
+
         private void Awake()
         {
             // Setting up references.
@@ -27,8 +29,15 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            DontDestroyOnLoad(this);
         }
 
+        public void loseLife()
+        {
+            loseLives++;
+        }
+
+       
 
         private void FixedUpdate()
         {
